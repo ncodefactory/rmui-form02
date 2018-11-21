b7258ec04f11ff36dfb75a642904150b3df508dd
Bartosz Solarski <bartosz.solarski@hotmail.com>
2018-11-21 01:44:28 +0100

    messaging - first blood

--- src/App.js
+++ src/App.js
@@ -2,10 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      args: [],
+      channel: null,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('message', this.handleParentMessage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleParentMessage);
+  }
+
+  handleParentMessage = (e) => {
+    if (
+      e.data.type
+      && e.data.type === 'rmui-forms-comm'
+      && e.data.direction === 'from-parent-to-child'
+    ) {
+      this.setState({ args: e.data.args, channel: e.data.channel });
+    }
+  };
+
+  sendToParent = (data) => {
+    const { channel } = this.state;
+    window.parent.postMessage(
+      {
+        channel,
+        args: data,
+        type: 'rmui-forms-comm',
+        direction: 'from-child-to-parent',
+      },
+      '*',
+    );
+  };
+
   render() {
+    const { args } = this.state;
+    const info = `${args.length} args recieved from parent.`;
     return (
       <div className="App">
         <h1>rmui-form02</h1>
+        <h2>{info}</h2>
+        <h3>send back to parent</h3>
+        <button type="button" onClick={() => { this.sendToParent([args[0]]); }}>
+          Arg0
+        </button>
+        <button type="button" onClick={() => { this.sendToParent([args[1]]); }}>
+          Arg1
+        </button>
+        <button type="button" onClick={() => { this.sendToParent([args[2]]); }}>
+          Arg2
+        </button>
       </div>
     );
   }
